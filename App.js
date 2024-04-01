1acc27b5ea1d8fec3742f07c436e60dbcb3177ce
HwiJoon Lee <107892477+Halfgear@users.noreply.github.com>
2024-04-01 04:18:59 -0400

    finsihed up to part 3

--- App.js
+++ App.js
@@ -4,10 +4,16 @@ import Lab5 from "./Lab5.js";
 import cors from "cors";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
+import UserRoutes from "./Users/routes.js";
+
+import mongoose from "mongoose";
+
 
 const app = express()
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 app.use(cors());
 app.use(express.json());
+UserRoutes(app);
 CourseRoutes(app);
 ModuleRoutes(app);
 Lab5(app);
---
+++ README.md
@@ -0,0 +1,1 @@
+nodemon App.js
