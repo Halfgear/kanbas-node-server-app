f89f7658649a7a704581ce52172594565c138e07
HwiJoon Lee <107892477+Halfgear@users.noreply.github.com>
2024-04-04 01:55:56 -0400

    removed for now

--- Users/routes.js
+++ Users/routes.js
@@ -1,8 +1,5 @@
 import * as dao from "./dao.js";
 let currentUser = null;
-export default function QuizRoutes(app){
-
-}
 
 export default function UserRoutes(app) {
     const createUser = async (req, res) => {
